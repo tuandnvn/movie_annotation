8d616ff1db246e2e83d69260bd5f2bcd91739bd7
Tuan Do <tuandn@brandeis.edu>
2017-03-02 20:24:52 -0500

    Update clean tuple

--- process_code/clean_tuple.py
+++ process_code/clean_tuple.py
@@ -3,19 +3,81 @@ Created on Mar 1, 2017
 
 @author: Tuan
 '''
-import argparse
+'''
+Using a spacy parser, turn a tuple form into a lemmatized form
+Example is:
+( swarm,  continues,  null, on,  buildings )
+-> ( swarm,  continue,  null, on,  buildings ) 
+Note that buildings should be kept intact because 
+buildings appear a lots in the training dataset -> other words might be similar
+are "windows", "doors", "people"
+
+We need to feed in a counter of words because 
+we want to keep the non-lemmatized form of words if its appears a lots)
+- For example: 
+        + plural forms of common words 
+        + adverbs (sharply, directly etc.)
+- These are considered as different words, so hopefully that could improve the performance
+Parameters
+----------
+spacy_parser: a spacy parser, in this case should be an english parser
+counter: counter of terms in the training data 
+                       
+Returns
+-------
+'''
+
+from collections import Counter
 import re
+
+import numpy as np
+import argparse
+
 from spacy.en import English
 
-def process ( spacy_parser, subject, verb, obj, prep, prepDep ):
-    simple_sentence = subject + verb + obj + prep + prepDep
+def process ( spacy_parser, counter, subject, verb, obj, prep, prepDep ):
+    tuple =  [subject, verb, obj, prep, prepDep]
+    subject, verb, obj, prep, prepDep = subject.lower(), verb.lower(), obj.lower(), prep.lower(), prepDep.lower()
     
-    lemma_ids = []
+    simple_sentence = '%s %s %s %s %s' % (subject, verb, obj, prep, prepDep)
+    simple_sentence = unicode(simple_sentence.lower())
+    
+    # For each word, count the tokens
+    no_tokens = [len(t.split()) for t in [subject, verb, obj, prep, prepDep]]
+    
+    lemmatized_words = []
+    
+    # Token counter
+    token_counter = 0
+    
+    # Word counter
+    word_counter = 0
+    
+    lemma = []
     for token in spacy_parser(simple_sentence):
-        lemma_ids.append(token.lemma)
+        token_counter += 1
+        lemma.append(token.lemma_)
         
-    # By default, just remove this tuple
-    return (None, None, None, None, None)
+        if token_counter == no_tokens[word_counter]:
+            lemmatized_words.append(' '.join(lemma))
+            
+            # Reset all variables
+            word_counter += 1
+            token_counter = 0
+            lemma = []
+    
+    result = (None, None, None, None, None)
+    result[1] = lemmatized_words[1]
+    for i in [0, 2, 3, 4]:
+        # The count of this form is significant in compare to the lemmatized form
+        # Such as buildings vs building
+        if (counter[tuple[i]] > 10 and float(counter[tuple[i]])/ float(counter[lemmatized_words[i]]) > 0.2) or \
+            (counter[tuple[i]] > 50 and float(counter[tuple[i]])/ float(counter[lemmatized_words[i]]) > 0.1):
+            result[i] = tuple[i]
+        else:
+            result[i] = lemmatized_words[i]
+            
+    return result
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='A script to clean tuple forms to either simpler form(lemmatize) or to remove rare words')
@@ -34,12 +96,26 @@ if __name__ == "__main__":
     p = re.compile('<?P<sentence>\d+)\s+(?P<subject>\w+,)(?P<verb>\w+,)(?P<object>\w+,)(?P<prep>\w+,)(?P<prepDep>\w+)')
     
     outputs = []
+    
+    
+    counter = Counter()
+    '''First pass, just do some statistics'''
     with open(input_file, 'r') as filehandler:
         for line in input_file:
             result = p.match( line )
             sentence = result.group('sentence')
             
-            value = process ( result.group('subject'), result.group('verb'), result.group('object'), result.group('prep'), result.group('prepDep'))
+            counter.update([result.group('subject').lower(), result.group('verb').lower(), 
+                            result.group('object').lower(), result.group('prep'.lower()), result.group('prepDep').lower()])
+    
+    hist = np.histogram(counter.values())
+    print hist
+    parser = English()
+    
+    '''Second pass, do the processing'''
+    with open(input_file, 'r') as filehandler:
+        for line in input_file:
+            value = process ( parser, counter, result.group('subject'), result.group('verb'), result.group('object'), result.group('prep'), result.group('prepDep'))
             if any(value):
                 outputs.append((sentence, value)) 
                 
--- process_code/dimension_reduction.py
+++ process_code/dimension_reduction.py
@@ -25,11 +25,11 @@ if __name__ == "__main__":
                                 help = "Process INPUT_DIR that stores the feature vectors. One feature vector file corresponds to one clip. \
                                 Generate a decomposition model, save it down to MODEL file. Only use clips in CLIP_NAME_FILE. Limit for the first n clips." )
     
-    parser.add_argument('-g', '--generate',  nargs = 5, metavar = ('OUTPUT_DIR', 'MODEL', 'CLIP_NAME_FILE', 'ALGORITHM', 'LIMIT'),
+    parser.add_argument('-g', '--generate',  nargs = 5, metavar = ('INPUT_DIR', 'OUTPUT_DIR', 'MODEL', 'CLIP_NAME_FILE', 'LIMIT'),
                                 help = "OUTPUT_DIR that stores the feature vectors (in reduced dimensions).\
-                                Loading ALGORITHM model from MODEL file." )
+                                Loading model from MODEL file." )
     
-    parser.add_argument('-v', '--verbose',  action='store_true', metavar = ('VERBOSE'),
+    parser.add_argument('-v', '--verbose',  action='store_true',
                                 help = "Whether to print out information." )
     
     args = parser.parse_args()
@@ -41,8 +41,8 @@ if __name__ == "__main__":
         model_file = args.process[1]
         clip_name_file = args.process[2]
         algorithm = args.process[3]
-        components = args.process[4]
-        limit = args.process[5]
+        components = int(args.process[4])
+        limit = int(args.process[5])
         
         feature_files = glob.glob(os.path.join(input_directory, '*.fv.npy') )
         feature_file_map = {}
@@ -54,65 +54,96 @@ if __name__ == "__main__":
         clip_names = []
         with open(clip_name_file, 'r') as file_handler:
             for line in file_handler:
-                clip_name, _ = line.split()
+                clip_name = line.split()[0]
                 clip_names.append(clip_name)
         
         trainer = None
         if algorithm == 'IPCA':
             trainer = IncrementalPCA(n_components=components)
         
+        # Stitch some array together before partial-fit
+        accu_array = []
+        accu_size = 5000
+        counter_size = 0 
+        
         if trainer != None:
             counter = 0
             
             for clip_name in clip_names[:limit]:
-                feature_file = feature_file_map[clip_name]
-                array = np.load(feature_file)
-                
-                trainer.partial_fit(array)
-                counter += 1
-                
-                if counter % 100 == 0:
-                    print counter
-                
-                if verbose:
-                    print clip_name
+                try:
+                    feature_file = feature_file_map[clip_name]
+                    
+                    array = np.load(feature_file)
+                    
+                    accu_array.append(array)
+                    counter_size += array.shape[0]
+                    
+                    if counter_size > accu_size:
+                        # Problem of partial fit a number of samples smaller than the number of components
+                        # Number of input features has changed from 10 to 20 between calls to partial_fit! Try setting n_components to a fixed value.
+                        # https://github.com/scikit-learn/scikit-learn/issues/6452
+                        #
+                        # Therefore the best way is probably to stitch some array together before partial-fit
+                        concatenate_accu_array = np.concatenate(accu_array, axis = 0)
+                        print 'Partial fit for array size =', concatenate_accu_array.shape
+                        trainer.partial_fit(concatenate_accu_array)
+                        
+                        accu_array = []
+                        counter_size = 0
+                        
+                    counter += 1
+                    
+                    if counter % 100 == 0:
+                        print counter
+                    
+                    if verbose:
+                        print clip_name
+                except:
+                    print 'Load file ', clip_name, ' has problem!!!'
+                    
+            concatenate_accu_array = np.concatenate(accu_array, axis = 0)
+            print 'Partial fit for array size =', concatenate_accu_array.shape
+            trainer.partial_fit(concatenate_accu_array)
             
             print 'Dump model to ', model_file
             # Save down model into model_file
             joblib.dump(trainer, model_file)
     
-    if args.generate and len(args.process) == 5:
-        output_directory = args.process[0]
-        model_file = args.process[1]
-        clip_name_file = args.process[2]
-        algorithm = args.process[3]
-        limit = args.process[4]
+    if args.generate and len(args.generate) == 5:
+        input_directory = args.generate[0]
+        output_directory = args.generate[1]
+        model_file = args.generate[2]
+        clip_name_file = args.generate[3]
+        limit = int(args.generate[4])
         
+        feature_files = glob.glob(os.path.join(input_directory, '*.fv.npy') )
+        feature_file_map = {}
+        
+        for feature_file in feature_files:
+            ff = feature_file.split("/")[-1]
+            feature_file_map[ff[:- len('.fv.npy')]] = feature_file
+            
         clip_names = []
         with open(clip_name_file, 'r') as file_handler:
             for line in file_handler:
-                clip_name, _ = line.split()
+                clip_name = line.split()[0]
                 clip_names.append(clip_name)
         
         trainer = joblib.load(model_file)
         
         if trainer != None:
             for clip_name in clip_names[:limit]:
-                feature_file = feature_file_map[clip_name]
-                array = np.load(feature_file)
-                
-                reduced_array = trainer.transform(array)
-                new_feature_file = os.path.join(output_directory, clip_name + '.rfv')
-                
-                if verbose:
-                    print new_feature_file, reduced_array.shape
-                np.save(new_feature_file, reduced_array)
-    
-    
-    
-    
-    
-        
-    
-    
-    
+                try:
+                    feature_file = feature_file_map[clip_name]
+                    
+                    array = np.load(feature_file)
+                    
+                    reduced_array = trainer.transform(array)
+                    new_feature_file = os.path.join(output_directory, clip_name + '.rfv')
+                    
+                    if verbose:
+                        print new_feature_file, reduced_array.shape
+                    np.save(new_feature_file, reduced_array)
+                except Exception as exc:
+                    print exc
+                    print 'Process file ', clip_name, ' has problem!!!'
