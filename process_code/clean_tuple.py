25f849ee0e6569c03580b514a21112e8e2728a9b
Tuan Do <tuandn@brandeis.edu>
2017-03-03 09:53:07 -0500

    Add logs for simplifying tuples

--- process_code/clean_tuple.py
+++ process_code/clean_tuple.py
@@ -35,49 +35,94 @@ import argparse
 
 from spacy.en import English
 
-def process ( spacy_parser, counter, subject, verb, obj, prep, prepDep ):
-    tuple =  [subject, verb, obj, prep, prepDep]
-    subject, verb, obj, prep, prepDep = subject.lower(), verb.lower(), obj.lower(), prep.lower(), prepDep.lower()
+def process ( spacy_parser, c, subject, verb, obj, prep, prepDep ):
+    result = ["null"] * 5
     
-    simple_sentence = '%s %s %s %s %s' % (subject, verb, obj, prep, prepDep)
-    simple_sentence = unicode(simple_sentence.lower())
-    
-    # For each word, count the tokens
-    no_tokens = [len(t.split()) for t in [subject, verb, obj, prep, prepDep]]
-    
-    lemmatized_words = []
-    
-    # Token counter
-    token_counter = 0
-    
-    # Word counter
-    word_counter = 0
-    
-    lemma = []
-    for token in spacy_parser(simple_sentence):
-        token_counter += 1
-        lemma.append(token.lemma_)
+    try:
+        ts =  [t.lower() if not t.isspace() else 'null' for t in [subject, verb, obj, prep, prepDep]]
         
-        if token_counter == no_tokens[word_counter]:
-            lemmatized_words.append(' '.join(lemma))
+        simple_sentence = unicode('%s %s %s %s %s' % tuple(ts))
+        
+        # For each word, count the tokens
+        no_tokens = [len(t.split()) for t in ts]
+        
+        lemmatized_words = []
+        
+        # Token counter
+        token_counter = 0
+        
+        # Word counter
+        word_counter = 0
+        
+        lemma = []
+        for token in spacy_parser(simple_sentence):
+            token_counter += 1
+            lemma.append(token.lemma_)
             
-            # Reset all variables
-            word_counter += 1
-            token_counter = 0
-            lemma = []
-    
-    result = (None, None, None, None, None)
-    result[1] = lemmatized_words[1]
-    for i in [0, 2, 3, 4]:
-        # The count of this form is significant in compare to the lemmatized form
-        # Such as buildings vs building
-        if (counter[tuple[i]] > 10 and float(counter[tuple[i]])/ float(counter[lemmatized_words[i]]) > 0.2) or \
-            (counter[tuple[i]] > 50 and float(counter[tuple[i]])/ float(counter[lemmatized_words[i]]) > 0.1):
-            result[i] = tuple[i]
-        else:
-            result[i] = lemmatized_words[i]
+            if token_counter == no_tokens[word_counter]:
+                lemmatized_words.append(' '.join(lemma))
+                
+                # Reset all variables
+                word_counter += 1
+                token_counter = 0
+                lemma = []
+        
+        verb = lemmatized_words[1]
+        
+        if verb[:3] == 'be ':
+            verb = verb[3:]
+            
+        result[1] = verb
+        for i in [0, 2, 3, 4]:
+            # The count of this form is significant in compare to the lemmatized form
+            # Such as buildings vs building
+            if counter[lemmatized_words[i]] == 0 or \
+                (c[ts[i]] > 10 and float(c[ts[i]])/ float(c[lemmatized_words[i]]) > 0.2) or \
+                (c[ts[i]] > 50 and float(c[ts[i]])/ float(c[lemmatized_words[i]]) > 0.1):
+                result[i] = str(ts[i])
+            else:
+                result[i] = str(lemmatized_words[i])
+    except Exception as e:
+        print e
+        print 'Problem in sentence ' + simple_sentence
+        for token in spacy_parser(simple_sentence):
+            print token.lemma_
             
     return result
+
+'''
+tuples = [(sentence, subject, verb, obj, prep, prepDep) ] 
+'''
+def printStatistics( tuples ):
+    counter = Counter()
+    slot_counters = [Counter() for _ in xrange(5)]
+    
+    
+    for t in tuples:
+        counter.update(t)
+        
+        for i in xrange(5):
+            # Start from 1
+            slot_counters[i].update((t[i + 1], ))
+        
+    print 'Number of unique words = ', len(counter)
+    for value in [1, 5, 10, 20, 50, 100, 200, 500, 1000]:
+        l = [t for t in counter if counter[t] > value]
+        print 'Number of words with frequency > ', value, ' = ', len(l)
+        if value == 100:
+            print sorted((counter[u], u) for u in l)
+            
+    for i, t in enumerate(['subject', 'verb', 'object', 'prep', 'prepDep']):
+        print '----------------------------------------------------------------\n'
+        
+        print 'Number of unique words for ', t, ' = ', len(slot_counters[i])
+        for value in [1, 5, 10, 20, 50, 100, 200, 500, 1000]:
+            l = [t for t in slot_counters[i] if slot_counters[i][t] > value]
+            print 'Number of words with frequency > ', value, ' = ', len(l)
+            if value == 100:
+                print sorted((slot_counters[i][u], u) for u in l)
+                
+    return counter, slot_counters
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='A script to clean tuple forms to either simpler form(lemmatize) or to remove rare words')
@@ -88,39 +133,60 @@ if __name__ == "__main__":
     parser.add_argument('-o', '--output_file', action='store', metavar = ('OUTPUT_FILE'),
                                 help = "Output file that store tuples." )
     
+    parser.add_argument('-v', '--verbose',  action='store_true',
+                                help = "Whether to print out information." )
+    
+    parser.add_argument('-l', '--limit',  action='store', metavar = ('LIMIT'),
+                                help = "Limit to the first LIMIT lines." )
+    
     args = parser.parse_args()
     
     input_file = args.input_file
     output_file = args.output_file
+    verbose = args.verbose
+    limit = int(args.limit)
     
-    p = re.compile('<?P<sentence>\d+)\s+(?P<subject>\w+,)(?P<verb>\w+,)(?P<object>\w+,)(?P<prep>\w+,)(?P<prepDep>\w+)')
+    p = re.compile('(?P<sentence>\d+)(\t\s*)?(?P<subject>[\s\w]+)(,\s*)?(?P<verb>[\s\w]+)(,\s*)?(?P<object>[\s\w]+)(,\s*)?(?P<prep>[\s\w]+)?(,\s*)?(?P<prepDep>[\s\w]+)?')
     
     outputs = []
     
     
-    counter = Counter()
+    
+    lines = []
     '''First pass, just do some statistics'''
     with open(input_file, 'r') as filehandler:
-        for line in input_file:
-            result = p.match( line )
-            sentence = result.group('sentence')
-            
-            counter.update([result.group('subject').lower(), result.group('verb').lower(), 
-                            result.group('object').lower(), result.group('prep'.lower()), result.group('prepDep').lower()])
+        for line in filehandler:
+            lines.append(line.strip())
     
-    hist = np.histogram(counter.values())
-    print hist
+    parse_tuples = []
+    
+    for line in lines:
+        result = p.match( line )
+        sentence = result.group('sentence')
+        
+        values = [ result.group(t).lower() if result.group(t) != None else 'null' for t in ['subject', 'verb', 'object', 'prep', 'prepDep'] ]
+        parse_tuples.append( tuple([sentence] + values ))
+    
+    
+    counter, slot_counters = printStatistics(parse_tuples)
+        
     parser = English()
     
+    processed_tuples = []
+    
     '''Second pass, do the processing'''
-    with open(input_file, 'r') as filehandler:
-        for line in input_file:
-            value = process ( parser, counter, result.group('subject'), result.group('verb'), result.group('object'), result.group('prep'), result.group('prepDep'))
-            if any(value):
-                outputs.append((sentence, value)) 
+    for sentence, subject, verb, obj, prep, prepDep  in parse_tuples[:limit]:
+        values = process ( parser, counter, subject, verb, obj, prep, prepDep )
+        
+        if verbose:
+            print sentence, values
+        if any(values):
+            processed_tuples.append( tuple([sentence] + values ))
+            
+            
+    printStatistics(processed_tuples)
                 
     with open(output_file, 'w') as filehandler:
-        for sentence, value in outputs:
-            filehandler.write(sentence)
-            filehandler.write('%10s,%10s,%10s,%10s,%10s' % value)
+        for t in processed_tuples:
+            filehandler.write('%s\t%10s,%10s,%10s,%10s,%10s' % t)
             filehandler.write('\n')
